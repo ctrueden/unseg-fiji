ad690bd0d8f1ef9109c29d6c9706d5e788b41c53
Curtis Rueden <ctrueden@wisc.edu>
2025-11-18 13:43:57 -0600

    Build the Appose environment

--- Unseg_Fiji.groovy
+++ Unseg_Fiji.groovy
@@ -2,4 +2,31 @@
 #@output Img nuclei
 #@output Img cells
 
-println(image)
+import org.apposed.appose.Appose
+
+println("== BUILDING ENVIRONMENT ==")
+pixiToml = """
+[workspace]
+authors = ["Curtis Rueden <ctrueden@wisc.edu>"]
+channels = ["conda-forge"]
+name = "unseg-fiji"
+platforms = ["linux-64"]
+version = "0.1.0"
+
+[tasks]
+
+[dependencies]
+python = "3.9.*"
+appose = "==0.7.2"
+numpy = "==1.24.3"
+matplotlib = "==3.7.1"
+scikit-image = "==0.20.0"
+scikit-learn = "==1.2.2"
+scipy = "==1.9.1"
+
+[pypi-dependencies]
+opencv-python-headless = "==4.7.0.72"
+"""
+
+env = Appose.pixi().content(pixiToml).logDebug().build()
+println("Environment build complete: ${env.base()}")
